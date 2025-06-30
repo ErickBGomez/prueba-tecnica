ac68f20c31da6c8bac28b9d86c1a7662c645bbd8
ErickBGomez <erickbgomez.dev@gmail.com>
2025-06-30 10:05:50 -0600

    Validate form data

--- prueba-tecnica/src/components/AddActivityForm/AddActivityForm.jsx
+++ prueba-tecnica/src/components/AddActivityForm/AddActivityForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./AddActivityForm.module.scss";
 
 const days = [
@@ -11,9 +12,45 @@ const days = [
 ];
 
 const AddActivityForm = () => {
+  const [errors, setErrors] = useState({});
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    duration: "",
+    day: "monday",
+  });
+
+  const validateForm = () => {
+    // Every field is required
+    if (!values.name.trim()) setErrors({ ...errors, name: "Name is required" });
+
+    if (!values.description.trim())
+      setErrors({ ...errors, description: "Description is required" });
+
+    if (!values.duration.trim())
+      setErrors({ ...errors, duration: "Duration is required" });
+
+    if (values.duration <= 0)
+      // Duration must be greater than 0
+      setErrors({ ...errors, duration: "Duration must be greater than 0" });
+
+    if (!values.day) setErrors({ ...errors, day: "Day is required" });
+
+    return Object.keys(errors).length === 0;
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Handle form submission logic here
+
+    setErrors({}); // Reset errors
+
+    // Do not submit if form has errors
+    if (!validateForm()) {
+      alert("Your form has errors. Please fix them before submitting.");
+      return;
+    }
+
+    // Set new value to activities state from context + reducer
   };
 
   return (
@@ -27,6 +64,8 @@ const AddActivityForm = () => {
             id="name"
             placeholder="ex: My Activity"
             required
+            value={values.name}
+            onChange={(e) => setValues({ ...values, name: e.target.value })}
           />
         </div>
 
@@ -35,6 +74,10 @@ const AddActivityForm = () => {
           <textarea
             required
             placeholder="ex: This is a description of the activity"
+            value={values.description}
+            onChange={(e) =>
+              setValues({ ...values, description: e.target.value })
+            }
           ></textarea>
         </div>
 
@@ -46,12 +89,20 @@ const AddActivityForm = () => {
             id="duration"
             placeholder="ex: 60"
             required
+            value={values.duration}
+            onChange={(e) => setValues({ ...values, duration: e.target.value })}
           />
         </div>
 
         <div className={styles.inputGroup}>
           <label htmlFor="day">Day</label>
-          <select name="day" id="day" required>
+          <select
+            name="day"
+            id="day"
+            required
+            value={values.day}
+            onChange={(e) => setValues({ ...values, day: e.target.value })}
+          >
             {days.map((day, index) => (
               <option key={index} value={day.value}>
                 {day.label}
