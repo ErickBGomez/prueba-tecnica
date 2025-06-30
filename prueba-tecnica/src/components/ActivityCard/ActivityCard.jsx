fc28c4b5f698e24088a48117840b2e2dc4af78e6
ErickBGomez <erickbgomez.dev@gmail.com>
2025-06-30 12:21:03 -0600

    Finish technical test

--- prueba-tecnica/src/components/ActivityCard/ActivityCard.jsx
+++ prueba-tecnica/src/components/ActivityCard/ActivityCard.jsx
@@ -13,12 +13,18 @@ const ActivityCard = ({ id, name, description, duration }) => {
     }
   };
 
+  const handleReorder = (direction) => {
+    dispatch({ type: "Reorder", id, direction });
+  };
+
   return (
     <div className={styles.activity}>
       <div className={styles.actions}>
         <button className={styles.deleteButton} onClick={handleDelete}>
           Delete
         </button>
+        <button onClick={() => handleReorder(-1)}>Up</button>
+        <button onClick={() => handleReorder(1)}>Down</button>
       </div>
       <h3 className={styles.name}>{name}</h3>
       <p className={styles.description}>{description}</p>
--- prueba-tecnica/src/reducer/activitiesReducer.jsx
+++ prueba-tecnica/src/reducer/activitiesReducer.jsx
@@ -12,6 +12,26 @@ const activitiesReducer = (state, action) => {
       return state.filter((activity) => activity.id !== action.id);
     }
 
+    case "Reorder": {
+      const [...activities] = state;
+      const { direction, id } = action;
+      let temp;
+
+      if (
+        direction + activities.length - 1 >= activities.length ||
+        direction + activities.length - 1 < 0
+      )
+        return [...state];
+
+      const index = activities.findIndex((activity) => activity.id === id);
+
+      temp = activities[index + direction];
+      activities[index + direction] = activities[index];
+      activities[index] = temp;
+
+      return [...state, activities];
+    }
+
     default:
   }
 };
