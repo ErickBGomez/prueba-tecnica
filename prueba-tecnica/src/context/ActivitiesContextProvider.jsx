0f0adc32f63324081bdfb1446558f2df56a3a35e
ErickBGomez <erickbgomez.dev@gmail.com>
2025-06-30 10:25:49 -0600

    Use context and reducer to save activities

--- prueba-tecnica/src/context/ActivitiesContextProvider.jsx
+++ prueba-tecnica/src/context/ActivitiesContextProvider.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useReducer } from "react";
+import activitiesReducer from "../reducer/activitiesReducer";
 import ActivityContext from "./ActivitiesContext";
+
+const loadLocalStorage = () => {
+  const activities = localStorage.getItem("activities");
+  return activities ? JSON.parse(activities) : [];
+};
+
+const saveToLocalStorage = (activities) => {
+  localStorage.setItem("activities", JSON.stringify(activities));
+};
 
 const ActivitiesContextProvider = ({ children }) => {
   // Set reducer here
+  const [state, dispatch] = useReducer(activitiesReducer, loadLocalStorage());
+
+  // Save to local storage when state changes
+  useEffect(() => {
+    saveToLocalStorage(state);
+  }, [state]);
 
   return (
-    <ActivityContext.Provider value={{}}>{children}</ActivityContext.Provider>
+    <ActivityContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ActivityContext.Provider>
   );
 };
 
---
+++ prueba-tecnica/src/reducer/activitiesReducer.jsx
@@ -0,0 +1,20 @@
+const activitiesReducer = (state, action) => {
+  switch (action.type) {
+    case "Add": {
+      if (state.some((activity) => activity.id === action.newActivity.id))
+        return;
+
+      return [...state, ...action.newActivity];
+    }
+    case "Remove": {
+      if (!state.some((activity) => activity.id === action.newActivity.id))
+        return;
+
+      return state.filter((activity) => activity.id !== action.newActivity.id);
+    }
+
+    default:
+  }
+};
+
+export default activitiesReducer;
